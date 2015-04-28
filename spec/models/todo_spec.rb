d77dd86569ca0d88724ab830e8ae69c0b03488e3
Lydias303 <shaferlydia@yahoo.com>
2015-04-27 22:48:29 -0600

    adde todo validation tests

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  test_todo = Todo.new(todo_item: "Make a todo rails app!",
-                       completed: false,
-                       priority: true)
+  lydias_todo = Todo.new(todo_item: "Make a todo rails app!",
+                         completed: false,
+                         priority: true)
+
+  nathans_todo = Todo.new(todo_item: "Wash the car")
 
   it "is valid" do
     expect(test_todo).to be_valid
@@ -12,5 +14,9 @@ RSpec.describe Todo, type: :model do
   it "is not valid without a todo item" do
     test_todo.todo_item = ""
     expect(test_todo).to be_invalid
+  end
+
+  it "has a default completed status of false" do
+    expect(nathans_todo.completed).to eq(false)
   end
 end
