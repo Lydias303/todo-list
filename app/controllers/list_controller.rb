aca2c18f31850f1641f494926b446d4f10a3a5d2
Lydias303 <shaferlydia@yahoo.com>
2015-04-28 20:06:18 -0600

    added list resources to routes, added create method in lsit controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,5 +1,11 @@
 class ListController < ApplicationController
   def index
     @lists = List.all
+    @list  = List.new
+  end
+
+  def create
+    List.create(title: params[:list][:title])
+    redirect_to root_path
   end
 end
