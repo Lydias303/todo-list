b6048f77b2fc90e19ad0e171c4b267d80aa360a1
Lydias303 <shaferlydia@yahoo.com>
2015-04-29 17:43:44 -0600

    pulled out glyphcons from index as they are being appended individually with javascript

---
+++ spec/feature/todo_feature_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe "an authenticated user" do
+  include Capybara::DSL
+
+  before(:each) do
+    visit '/'
+  end
+
+
+  it "can create a todo associated with a list" do
+    expect(page).to have_content("Create A New Todo List")
+
+    within(".list-create") do
+      fill_in "list[title]", with: "New List"
+      click_button "Create List"
+    end
+    expect(page).to have_content("New List")
+
+    within('.bhoechie-tab-menu') do
+      first('.list-title-display').click
+    end
+
+    within(".input-group-btn") do
+      fill_in "new-todo-input", with: "Go to the Store"
+      click_button "add-todo"
+    end
+    save_and_open_page
+    expect(Todo.count).to eq(1)
+  end
+end
