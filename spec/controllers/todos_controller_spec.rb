5ba2f4c5bb21dfaf4214b25b1be81d22ba755346
Lydias303 <shaferlydia@yahoo.com>
2015-04-29 15:33:25 -0600

    mended controller todo test in reference to new todo create

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TodosController, :type => :controller do
 
   it "can create a todo on a list" do
     list = create(:list)
-    post :create, todo: { list_id: list.id, todo_item: "Do dishes" }
+    post :create, todo: { list: list.id, text: "Do dishes" }
 
     assert_redirected_to root_path
     expect(list.todos.count).to eq(1)
